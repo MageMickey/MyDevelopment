1d3ef002b496cc1d6461c5e6b22d2c14808a5b41
MageMickey <1164087865@qq.com>
2019-04-30 23:50:07 +0800

    createMenu

--- src/main/java/com/wx/demo/constants/WechatConstants.java
+++ src/main/java/com/wx/demo/constants/WechatConstants.java
@@ -1,22 +1,14 @@
 package com.wx.demo.constants;
 
-import com.wx.demo.model.Menu;
 import com.wx.demo.model.timing.AccessTokenThread;
-import com.wx.demo.utils.WeixinUtil;
-import net.sf.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 //静态常量类
 public class WechatConstants {
     //公众号appid
-    public static final String APPID = "wx860bf6a8512b89e2";
+    public static final String APPID = "你的appid";
 
     //公众号appsecert
-    public static final String APPSECRET = "dfc8670351e2c467e00c159eab060349";
+    public static final String APPSECRET = "你的appsecert";
 
     //获取access_token_Url
     private static String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
